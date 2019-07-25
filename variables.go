640cbac532f9fb73086ff1baec3321ed19821d54
Potapy4 <dev.potapy4@hotmail.com>
2019-07-25 21:40:40 +0300

    Learning variables

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare variables with type
+	var a bool
+	var b int = 42
+
+	// short variable declaration
+	c := "Hello World!"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
